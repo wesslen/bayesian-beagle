7843ea3fd046d06388f2a0267f257321073ec31a
Ryan Wesslen <ryan@explosion.ai>
2023-12-31 15:51:12 -0500

    add logging, skip previous built in summarizer.py

--- scripts/summarizer.py
+++ scripts/summarizer.py
@@ -5,8 +5,14 @@ from openai import OpenAI
 import arxiv
 import json
 from pathlib import Path
+import logging
 
 app = typer.Typer()
+
+# Initialize logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def is_valid_arxiv_id(arxiv_id: str) -> bool:
@@ -15,35 +21,13 @@ def is_valid_arxiv_id(arxiv_id: str) -> bool:
     try:
         first_result = next(client.results(search_by_id))
         return True, first_result
-    except Exception:
+    except BaseException:
         return False, None
 
 
 def extract_text_from_html(html_content: str) -> str:
     soup = BeautifulSoup(html_content, "html.parser")
     return soup.get_text()
-
-
-def truncate_string(text, token_threshold):
-    """
-    Truncate a string after a specified number of word tokens.
-
-    :param text: The input string.
-    :param token_threshold: The maximum number of word tokens allowed.
-    :return: A truncated version of the string if it exceeds the token threshold.
-    """
-
-    # Split the text into words (tokens)
-    tokens = text.split()
-
-    # Check if the number of tokens is greater than the threshold
-    if len(tokens) > token_threshold:
-        # Truncate the list of tokens and join back into a string
-        truncated_text = " ".join(tokens[:token_threshold])
-        return truncated_text
-    else:
-        # If the text is within the limit, return it as is
-        return text
 
 
 def remove_double_quotes(input_string):
@@ -81,7 +65,6 @@ def summarize_text(text: str) -> str:
 def tldr_title(text: str) -> str:
     client = OpenAI()
 
-    # TODO: Generalize this to allow different models and prompts. This should be appended to each record.
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         messages=[
@@ -111,19 +94,34 @@ def summarize(input_jsonl: str, output_file_path: str = "data/output.jsonl"):
     output_file_path (str): Output JSONL file path.
     """
     output_path = Path(output_file_path)
-    # TODO: check if output_path exists, if so, read it in. check it includes "id" key. raise error if not.
+
+    # Check if output_path exists and read processed IDs
+    existing_ids = set()
+    if output_path.is_file():
+        with output_path.open("r") as output_file:
+            for line in output_file:
+                try:
+                    existing_data = json.loads(line)
+                    existing_ids.add(existing_data["id"])
+                except json.JSONDecodeError as e:
+                    logging.error("Error reading output file: %s", e)
 
     with open(input_jsonl, "r") as file:
         for line in file:
             data = json.loads(line)
-
-            # TODO: if output_path exists, check "id" versus data. if found, skip with continue.
             arxiv_id = data["id"]
             categories = data["categories"]
 
+            # Skip if already processed
+            if arxiv_id in existing_ids:
+                logging.info(
+                    f"Arxiv ID {arxiv_id} already processed. Skipping."
+                )
+                continue
+
             valid, first_result = is_valid_arxiv_id(arxiv_id)
             if not valid:
-                typer.echo(f"Invalid Arxiv ID: {arxiv_id}")
+                logging.info(f"Invalid Arxiv ID: {arxiv_id}")
                 continue
 
             url = f"https://browse.arxiv.org/html/{arxiv_id}"
@@ -132,10 +130,10 @@ def summarize(input_jsonl: str, output_file_path: str = "data/output.jsonl"):
 
             word_count = count_words(html_content)
             if word_count > 15000:
-                typer.echo(
-                    f"Warning: HTML content for {arxiv_id} exceeds 15,000 tokens. Truncating."
+                logging.info(
+                    f"Warning: HTML content for {arxiv_id} exceeds 15,000 tokens. Skipping."
                 )
-                html_content = truncate_string(html_content)
+                continue
 
             text = extract_text_from_html(html_content)
             summary = summarize_text(text)
@@ -152,14 +150,13 @@ def summarize(input_jsonl: str, output_file_path: str = "data/output.jsonl"):
                     "publish_date": first_result.published.strftime(
                         "%Y-%m-%d"
                     ),
-                    "truncated": True if word_count > 15000 else False,
                 },
             }
 
             with output_path.open("a") as output_file:
                 output_file.write(json.dumps(output_data) + "\n")
 
-            typer.echo(f"Summary for {arxiv_id} written to {output_path}")
+            logging.info(f"Summary for {arxiv_id} written to {output_path}")
 
 
 if __name__ == "__main__":
